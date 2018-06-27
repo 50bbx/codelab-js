2ab35263f66c197215ee86ce322b2bb3ee5c604d
Leonardo Stenico <leonardostenico@gmail.com>
2018-06-28 00:25:51 +0200

    Added comment to PREFIX

--- problems/PREFIX.js
+++ problems/PREFIX.js
@@ -1,3 +1,15 @@
+/* 
+ * Solution to CodeLab problem PREFIX
+ * 
+ * You can find the problem here: https://codelab.interviewbit.com/problems/prefix/
+ * 
+ * This source code is licensed under the GNU v3 GENERAL PUBLIC LICENSE found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ * https://github.com/50bbx/codelab-js
+ */
+
+
 const input =  ['zebra', 'dog', 'duck', 'dove'];
 
 const m = new Map()
@@ -30,4 +42,4 @@ for(let str of input) {
     }
 }
 
-console.log(prefixes)
+console.log(prefixes)\
