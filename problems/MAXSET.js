ea6b8c6342779c59f2b1d83e52bab53ee8bfd335
Leonardo Stenico <leonardostenico@gmail.com>
2018-07-22 18:15:28 +0200

    Added solution to MAXSET problem

---
+++ problems/MAXSET.js
@@ -0,0 +1,40 @@
+function splitArr(arr) {
+
+  let index = 0
+  const splittedArray = [[]]
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] < 0) {
+      index++
+      splittedArray[index] = []
+      continue
+    }
+
+    splittedArray[index].push(arr[i])
+  }
+
+  let sum = 0
+  let winner = 0
+  for( let j = 0; j < splittedArray.length; j++) {
+    const segment = splittedArray[j]
+
+    const segmentSum = segment.reduce((acc, el) => {
+      return acc + el
+    }, 0)
+    
+    if(segmentSum === sum) {
+      if(segment.length > splittedArr[winner].length) {
+        winner = j
+        continue
+      }
+    }
+
+    if(segmentSum > sum) {
+      winner = j
+      sum = segmentSum
+    }
+  }
+  
+  return splittedArray[winner]
+}
+
+console.log(splitArr([1,2,5,-7,2,3]))
